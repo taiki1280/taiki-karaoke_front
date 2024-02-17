4280a3f3807ad55ed996b4a6e6c3c110519b4b3c
taiki-dev <20001208taiki@gmail.com>
2024-02-17 23:10:17 +0900

    [update] get datas from django api use axios

--- src/score/components/ScoreContent.tsx
+++ src/score/components/ScoreContent.tsx
@@ -3,45 +3,37 @@ import { Link } from 'react-router-dom'
 
 interface Score {
   score: {
-    date_time: String,
-    point: Number,
-    total_point: Number,
-    id: Number,
-    song: {
-      d_artist_name: String
-      d_contents_name: String
-    }
+    // id: Number,
+    scoringDateTime: String,
+    totalPoints: String,
+    aiSensitivityBonus: String,
+    requestNo__artist__artistName: String
+    requestNo__contentsName: String
   }
 }
 
 const ScoreContent: FC<Score> = ({ score }) => {
-  // let col_span = 7
-  // <div className="list">
-  //   <span className="point">{score.point.toFixed(3)}</span>
-  //   <time className="date_time">{score.date_time}</time>
-  //   <span className="total_point">{score.total_point.toFixed(3)}</span>
-  //   <cite className="d_contents_name">{score.song.d_contents_name}</cite>
-  //   <Link className="detail" to={`score/${score.id}`}>詳細</Link>
-  //   <span className="d_artist_name">{score.song.d_artist_name}</span>
-  // </div>
+  const total_point: String = (Number(score.totalPoints) / 1000).toFixed(3)
+  // 素点を計算（現状、Aiボーナス点は計算内にしか使用されていない）
+  const point: String = ((Number(score.totalPoints) - Number(score.aiSensitivityBonus)) / 1000).toFixed(3)
   return (
     <div className="py-2 border-b-2">
-      <time className="date_time px-4 text-xs">{score.date_time}</time>
+      <time className="date_time px-4 text-xs">{score.scoringDateTime}</time>
       <div className='flex items-center justify-evenly'>
         <div className='basis-3/12 flex flex-col items-center md:basis-1/12'>
-          <span className='point text-lg'>{score.point.toFixed(3)}</span>
+          <span className='point text-lg'>{point}</span>
           <div className='flex items-center justify-around w-full'>
-            <span className="total_point basis-full text-center text-xs">{score.total_point.toFixed(3)}</span>
+            <span className="total_point basis-full text-center text-xs">{total_point}</span>
             <Link
               className="detail basis-full text-center text-xs text-blue-500 underline underline-offset-1"
-              to={`/score/${score.id}`}>
+              to={`/score/${score.scoringDateTime}`}>
               詳細
             </Link>
           </div>
         </div>
         <div className='basis-8/12 flex flex-col md:basis-10/12'>
-          <span className="d_artist_name text-xs">{score.song.d_artist_name}</span>
-          <cite className="d_contents_name text-lg">{score.song.d_contents_name}</cite>
+          <span className="d_artist_name text-xs">{score.requestNo__artist__artistName}</span>
+          <cite className="d_contents_name text-lg">{score.requestNo__contentsName}</cite>
         </div>
       </div>
     </div>
--- src/score/pages/ScoreList.tsx
+++ src/score/pages/ScoreList.tsx
@@ -1,54 +1,67 @@
+import React, { useState, useEffect } from 'react'
+import axios from 'axios'
 import ScoreContent from '../components/ScoreContent'
 import ScoreSearchForm from '../components/ScoreSearchForm'
 
 const ScoreList = () => {
 
-  let loaded = true
+  const [loaded_score_list, setLoadedScoreList] = useState(false)
+  const [score_list, setScoreList] = useState([])
+
+  useEffect(() => {
+    if (process.env.REACT_APP_DJANGO_APP_API_URL !== undefined)
+      axios.get(process.env.REACT_APP_DJANGO_APP_API_URL)
+        .then(res => {
+          setScoreList(res.data)
+          setLoadedScoreList(true)
+          console.log(res.data)
+        })
+    else
+      console.log('環境変数にDjangoアプリのURLを設定してください')
+  }, [])
 
   type Score = {
-    date_time: String,
-    point: Number,
-    total_point: Number,
-    id: Number,
-    song: {
-      d_artist_name: String
-      d_contents_name: String
-    }
+    totalPoints: String
+    scoringDateTime: String
+    aiSensitivityBonus: String
+    requestNo__artist__artistName: String
+    requestNo__contentsName: String
   }
 
-  let score: Score = {
-    date_time: '2022/11/05 20:00:00',
-    point: 10,
-    total_point: 15,
-    id: 1,
-    song: {
-      d_artist_name: 'dummy_artist_name',
-      d_contents_name: 'dummy_contents_name'
-    }
-  }
+  // let score: Score = {
+  //   date_time: '2022/11/05 20:00:00',
+  //   point: 10,
+  //   total_point: 15,
+  //   id: 1,
+  //   song: {
+  //     d_artist_name: 'dummy_artist_name',
+  //     d_contents_name: 'dummy_contents_name'
+  //   }
+  // }
 
-  var score_list: Score[] = []
+  // var score_list: Score[] = []
 
-  for (let i = 1; i <= Math.floor(Math.random() * 10000); i++) {
-    let copy_score = Object.assign({}, score)
-    let copy_song = Object.assign({}, score.song)
-    copy_song.d_artist_name += String(Math.floor(Math.random() * 100))
-    copy_song.d_contents_name += String(Math.floor(Math.random() * 100))
-    copy_score.song = copy_song
-    copy_score.id = i
-    copy_score.point = Math.random() * 100
-    copy_score.total_point = Math.random() * 100
-    score_list.push(copy_score)
-  }
+  // for (let i = 1; i <= Math.floor(Math.random() * 10000); i++) {
+  //   let copy_score = Object.assign({}, score)
+  //   let copy_song = Object.assign({}, score.song)
+  //   copy_song.d_artist_name += String(Math.floor(Math.random() * 100))
+  //   copy_song.d_contents_name += String(Math.floor(Math.random() * 100))
+  //   copy_score.song = copy_song
+  //   copy_score.id = i
+  //   copy_score.point = Math.random() * 100
+  //   copy_score.total_point = Math.random() * 100
+  //   score_list.push(copy_score)
+  // }
 
   return (
     <>
       {
-        loaded ?
+        loaded_score_list ?
           <>
             <ScoreSearchForm />
             <h1 className='text-xl'>検索結果は {Object.keys(score_list).length}件 でした～</h1>
-            {score_list.map((score: Score) => <ScoreContent score={score} key={String(score.id)} />)}
+            {/* {score_list.map(() => score)} */}
+            {score_list.map((score: Score) => <ScoreContent score={score} key={String(score.scoringDateTime)} />)}
           </>
           :
           <div>
